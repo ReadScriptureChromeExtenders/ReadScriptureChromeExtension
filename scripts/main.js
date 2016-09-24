f1da4dca7f1eb13e446e1a9b0b7f7f71d8225ffb
Steve Gough <sgough@nerdwallet.com>
2016-09-24 16:33:55 -0700

    add api for daily plan

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,92 @@ const getExodus = () => {
 }
 
 getExodus();
+
+
+window.api = (function () {
+    function Api (els) {
+         
+    }
+
+    var read = Array();
+    var watch = Array();
+    var pray = Array();
+     
+    var api = {
+        getDate: function (selector) {
+        	var now = new Date();
+			var start = new Date(now.getFullYear(), 0, 0);
+			var diff = now - start;
+			var oneDay = 1000 * 60 * 60 * 24;
+			var day = Math.floor(diff / oneDay);
+			return day;
+        },
+
+
+        getPlan: function (selector) {
+        	
+       		var url  = 'https://readscripture-api.herokuapp.com/api/v1/days/' + this.getDate();
+			  fetch(url)
+			  .then(res => res.json())
+			  .then(daysJSON => {
+			    console.log(daysJSON);
+			    read = Array();
+        		watch = Array();
+        		pray = Array();
+			    var numNodes = daysJSON.dayContents.length;
+			    for (var i = 0; i < numNodes; i++) {
+			    	var node = daysJSON.dayContents[i]; 
+		    		switch(node.type) {
+		    			case "read":
+		    				var tokens = node.passage.split(' ');
+		    				var book = tokens[0];
+		    				var chapters = tokens[1].split('-');
+		    				var item = {
+		    					'book' : book,
+		    					'start' : chapters[0]
+		    				};
+		    				if(chapters.length > 1) {
+		    					item.end = chapters[1];
+		    				}
+		    				read.push(item);
+		    			break;
+		    			case "watch":
+		    				watch.push(node);
+		    				break;
+		    			break;
+		    			case "pray":
+		    				var tokens = node.passage.split(' ');
+		    				var book = tokens[0];
+		    				var chapters = tokens[1].split('-');
+		    				var item = {
+		    					'book' : book,
+		    					'start' : chapters[0]
+		    				};
+		    				if(chapters.length > 1) {
+		    					item.end = chapters[1];
+		    				}
+		    				pray.push(item);
+		    			break;
+
+		    			default:
+		    		}
+			    }
+			  }).catch(err => console.error(err))
+	        },
+
+        getReadArray: function (selector) {
+        	return read;
+        },   
+
+        getPrayArray: function (selector) {
+        	return pray;
+        },   
+        getWatchArray: function (selector) {
+        	return watch;
+        },   
+    };
+     
+    return api;
+}());
+
+api.getPlan();
